4323a0d872efbe45fbcfde4930c22276a8ec4eb2
Tess <gard.tess@gmail.com>
2019-05-29 15:26:52 +1000

    projects page fixed up, added title, added social links

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 let aboutBtn = document.querySelector("#about-btn")
 let projBtn = document.querySelector("#project-btn")
 let contactBtn = document.querySelector("#contact-btn")
+let contactBtn2 = document.querySelector("#contact-btn-2")
+
 
 let about = document.querySelector(".about")
 let project = document.querySelector(".project")
 let contact = document.querySelector(".contact")
+let drawing = document.querySelector(".drawing")
+let rightBox = document.querySelector(".right-box")
 
 // clicking on ABOUT button
 aboutBtn.addEventListener("click", function (event) {
@@ -17,6 +21,10 @@ aboutBtn.addEventListener("click", function (event) {
 
   contact.classList.remove("contact-landing-page-right-inner-bottom")
   contact.classList.add("contact-landing-page-right-inner-bottom-hide")
+
+  drawing.classList.remove("landing-page-left-hide")
+  drawing.classList.add("landing-page-left")
+
 });
 
 
@@ -30,6 +38,15 @@ projBtn.addEventListener("click", function (event) {
   contact.classList.remove("contact-landing-page-right-inner-bottom")
   contact.classList.add("contact-landing-page-right-inner-bottom-hide")
 
+  drawing.classList.add("landing-page-left-hide")
+  drawing.classList.remove("landing-page-left")
+
+  rightBox.classList.add("landing-page-right-full")
+  rightBox.classList.remove("landing-page-right")
+
+
+
+
 });
 
 // clicking on CONTACT button
@@ -41,5 +58,23 @@ contactBtn.addEventListener("click", function (event) {
   about.classList.add("about-landing-page-right-inner-bottom-hide")
 
   project.classList.remove("project-landing-page-right-inner-bottom")
+
+  drawing.classList.add("landing-page-left-hide")
+  drawing.classList.remove("landing-page-left")
+
+});
+
+// clicking on CONTACT button 2
+contactBtn2.addEventListener("click", function (event) {
+
+  contact.classList.add("contact-landing-page-right-inner-bottom")
+  contact.classList.remove("contact-landing-page-right-inner-bottom-hide")
+
+  about.classList.add("about-landing-page-right-inner-bottom-hide")
+
+  project.classList.remove("project-landing-page-right-inner-bottom")
+
+  drawing.classList.add("landing-page-left-hide")
+  drawing.classList.remove("landing-page-left")
 
 });
